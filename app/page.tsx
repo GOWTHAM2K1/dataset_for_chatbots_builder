a1afe5c9c7cf737c4269a818f2d8e0f8c81d7747
GOWTHAM2K1 <k304418@gmail.com>
2023-11-08 01:00:55 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,118 @@
-import Image from 'next/image'
+"use client";
+
+import OpenAI from "openai";
+const openai = new OpenAI({ apiKey: `${process.env.NEXT_PUBLIC_OPENAI_KEY}`, dangerouslyAllowBrowser: true, });
 
 export default function Home() {
+
+
+  async function query(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = async (event) => {
+        const data = event.target.result;
+        try {
+          const response = await fetch(
+            "https://api-inference.huggingface.co/models/distil-whisper/distil-large-v2",
+            {
+              headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_HUGGING_FACE_API_KEY}` },
+              method: "POST",
+              body: data,
+            }
+          );
+          const result = await response.json();
+          resolve(result);
+        } catch (error) {
+          reject(error);
+           alert(`Error while sending the file:${error}`);
+        }
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  const create = async () => {
+    const inputElement = document.getElementById('fileupload');
+
+    if (inputElement instanceof HTMLInputElement && inputElement.files.length > 0) {
+      const selectedFile = inputElement.files[0];
+      query(selectedFile)
+        .then(async (response) => {
+          const value = await JSON.stringify(response);
+
+          const jsonObject = await JSON.parse(value);
+          conversion(jsonObject.text);
+        })
+        .catch((error) => {
+          alert(`Error while sending the file:${error}`);
+        });
+    } else {
+      console.error("No file selected or element with ID 'fileupload' not found.");
+    }
+  }
+
+  const conversion = async (value: object) => {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: `Convert the following sentence in question and answer format the statement is as follows make sure to return all the text-content and also add <br> tag after each question before answer : ${value}` }],
+      model: "gpt-3.5-turbo",
+
+    });
+    // console.log(value)
+    console.log(completion.choices[0].message.content)
+    const insertDataTag = document.getElementById('output_area')
+    insertDataTag!.innerHTML = `${completion.choices[0].message.content}`
+  }
+
+  const download = async () => {
+    const paragraph = document.getElementById('output_area');
+    const downloadButton = document.getElementById('downloadButton');
+    const textContent = paragraph.textContent;
+
+    // Create a Blob (Binary Large Object) from the text content
+    const blob = new Blob([textContent], { type: 'text/plain' });
+
+    // Create a temporary URL for the Blob
+    const url = URL.createObjectURL(blob);
+
+    // Create an anchor element for downloading
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'text-content.txt'; // Specify the file name
+    a.style.display = 'none';
+
+    // Append the anchor element to the document and trigger a click event
+    document.body.appendChild(a);
+    a.click();
+
+    // Remove the anchor element
+    document.body.removeChild(a);
+
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="z-10 h-screen w-screen p-8 bg-[#f4f6f7]">
+      <div className="w-full text-center "><h1 className=" font-semibold text-3xl text-slate-900 text-center mb-5 shadow-xl">Chat Bot Builder</h1></div>
+
+      <div className="flex max-md:flex-col gap-2">
+        <div className="flex-column left-0 h-full w-full md:w-[50%] bg-[#f4f6f7] text-center shadow-xl">
+          <h1 className=" font-semibold text-xl w-full text-slate-900 text-center mt-5 mb-3">Please upload the audio file</h1>
+          <div className="flex-col">
+            <input className="max-md:w-[80vw] overflow-hidden text-center justify-center bg-[#f4f6f7] border rounded-md border-[#8da9b6] m-2" type="file" id='fileupload'></input>
+            <button className="bg-[#f4f6f7] border rounded-md border-[#b4cdda] p-1 shadow-xl hover:bg-[#bcdbeb] m-4" onClick={create}>Submit</button>
+          </div>
+
+        </div>
+        <div className="flex-column left-0 h-full w-full md:w-[50%] bg-[#f4f6f7] text-center shadow-xl">
+          <h1 className=" font-semibold text-xl w-full text-slate-900 text-center mt-5 mb-2">Output from the uploaded file</h1>
+
+          <p className="text-left p-3 pl-8" id="output_area"></p>
+          <button id ="downloadButton" className="bg-[#f4f6f7] border rounded-md border-[#b4cdda] p-2 shadow-xl hover:bg-[#bcdbeb] m-4" onClick={download}>Download transcript</button>
+
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
